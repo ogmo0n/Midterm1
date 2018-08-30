4bdf839d3f67bfacd8bfaea9a5186c6fe720daf9
Christopher Odden <chrodd9604@students.ecpi.edu>
2018-08-30 18:45:46 -0400

    first commit

---
+++ CIS214_Midterm_Odden/Invoice.cs
@@ -0,0 +1,45 @@
+﻿// CIS214 Midterm: Let's get it!
+using System;
+
+namespace CIS214_Midterm_Odden
+{
+    class Invoice
+    {
+        // automatic properties for Invoice instance
+        public string PartNumber { get; set; } // part number
+        public string PartDescription { get; set; } // part description
+        public int Quantity { get; set; } // quantity of said part
+        public decimal Price { get; set; } // price of said part
+
+        // constructor initializes automatic properties
+        public Invoice(string num, string desc, int q, decimal p)
+        {
+            PartNumber = num; // set PartNumber to num
+            PartDescription = desc; // set PartDescription to desc
+
+            // if quantity is negative, set to zero
+            if (q < 0)
+                Quantity = 0;
+            else
+                Quantity = q; // set Quantity to q
+
+            // if price is negative, set to zero
+            if (p < 0)
+                Price = 0;
+            else
+                Price = p; // set Price to p
+        }
+
+        // calculates the invoice amount
+        public void GetInvoiceAmount()
+        {
+            decimal amount; // total price for quantity of part
+
+            // total amount equals quantity times price
+            amount = Quantity * Price;
+
+            // display total amount
+            Console.WriteLine("Total amount for this invoice is: {0:C}", amount);
+        }
+    }
+}
---
+++ CIS214_Midterm_Odden/InvoiceTest.cs
@@ -0,0 +1,20 @@
+﻿// InvoiceTest.cs
+// Creates object from class Invoice and demonstrates its capabilities
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIS214_Midterm_Odden
+{
+    class InvoiceTest
+    {
+        public static void Main(string[] args)
+        {
+            // create Invoice object
+            Invoice myInvoice = new Invoice("23413", "Hammer", 4, 11.99M);
+            Console.WriteLine("Part Number: {0}\nPart Description: {1}\nQuantity: {2}\nPrice: {3}",
+                myInvoice.PartNumber, myInvoice.PartDescription, myInvoice.Quantity, myInvoice.Price);
+            myInvoice.GetInvoiceAmount(); // get invoice amount for this instance
+        }
+    }
+}
